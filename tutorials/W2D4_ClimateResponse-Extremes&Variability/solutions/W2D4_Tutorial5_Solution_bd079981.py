a5d9f9cb8e269705fd47b891800ba28222f877f4
GitHub Action <action@github.com>
2024-02-24 06:23:22 +0000

    Process tutorial notebooks

---
+++ tutorials/W2D4_ClimateResponse-Extremes&Variability/solutions/W2D4_Tutorial5_Solution_bd079981.py
@@ -0,0 +1,3 @@
+"""
+1. There is a relatively large increase in the location parameter over time, and the scale parameter from 1961-1990 and 1991-2020 also increases. This is likely due to sea level rise. Furthermore, the variability around the mean is increasing. The shape parameter also increases over this time but as commented in the lecture this parameter is not usually reliably estimated from such a short time span.
+""";
