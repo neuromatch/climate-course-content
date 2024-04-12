429223df5b8bf8645714adcb4d43917dd01ef1ef
GitHub Action <action@github.com>
2024-04-12 22:37:10 +0000

    Process tutorial notebooks

--- tutorials/W2D3_FutureClimate-IPCCII&IIISocio-EconomicBasis/solutions/W2D3_Tutorial4_Solution_ef665345.py
+++
@@ -1,8 +0,0 @@
-
-language_code = "en"
-df_tmp = df.loc[df.lang == language_code, :].reset_index(drop=True)
-pd.options.display.max_rows = 100  # see up to 100 entries
-# widen how much text is presented of each tweet
-pd.options.display.max_colwidth = 250
-samples = df_tmp.sample(100)
-samples
