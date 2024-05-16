40d3a72dda1a807997ffa9f4013662f35915c797
GitHub Action <action@github.com>
2024-05-16 01:38:44 +0000

    Process tutorial notebooks

--- tutorials/W2D5_ClimateResponse-AdaptationImpact/solutions/W2D5_Tutorial2_Solution_cef4c6f7.py
+++ tutorials/W2D5_ClimateResponse-AdaptationImpact/solutions/W2D5_Tutorial2_Solution_cef4c6f7.py
@@ -10,26 +10,25 @@ def fit_and_visualize_rf(training_data, target):
         None
     """
 
-    # Fit the random forest regressor to the training data
-    rf_regressor.fit(training_data, target)
+    # fit the random forest regressor to the training data
+    _ = rf_regressor.fit(training_data, target)
 
-    # Print the R-squared score of the model
+    # print the R-squared score of the model
     print(rf_regressor.score(training_data, target))
 
-    # Predict the target variable using the trained model
+    # predict the target variable using the trained model
     predicted = rf_regressor.predict(training_data)
 
     # Create scatter plot
     plt.scatter(predicted,target,color='b',label='Comparison of Predicted and True Temperatures')
-    plt.plot([0,4],[0,4],color='r', label='Ideal Line') # Add a diagonal line for reference
+    plt.plot([0,4],[0,4],color='r', label='Ideal Line') # add a diagonal line for reference
     plt.xlabel('Predicted Temperatures (K)')
     plt.ylabel('True Temperatures (K)')
     plt.legend()
-    plt.title('Annual mean temperature anomaly', fontsize=14)
-    # Add a caption with adjusted y-coordinate to create space
+    plt.title('Annual mean temperature anomaly')
+    # add a caption with adjusted y-coordinate to create space
     caption_text = 'The anomalies are calculated by subtracting the annual means of the pre-industrial scenario from \nthe annual means of the respective scenario.'
-    plt.figtext(0.5, -0.03, caption_text, ha='center', fontsize=7)  # Adjusted y-coordinate to create space
+    plt.figtext(0.5, -0.03, caption_text, ha='center', fontsize=10)  # adjusted y-coordinate to create space
 
-## Uncomment the code below to test your function
-
-fit_and_visualize_rf(training_data, target)
+# test your function
+_ = fit_and_visualize_rf(training_data, target)
