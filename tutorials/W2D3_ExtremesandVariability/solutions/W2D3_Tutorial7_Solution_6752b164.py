99c8cafbb20a73f4e86fcf851d450b326947e5de
GitHub Action <action@github.com>
2024-07-01 18:20:50 +0000

    Process tutorial notebooks

---
+++ tutorials/W2D3_ExtremesandVariability/solutions/W2D3_Tutorial7_Solution_6752b164.py
@@ -0,0 +1,25 @@
+
+# instantiate a GEV distribution
+law_ns_scale = sd.GEV()
+
+# fit the GEV to the data, while specifying that the scale parameter ('scale') is meant to be a covariate ('_c') of the time axis (data.index)
+_ = law_ns_scale.fit(data.ssh.values, c_scale=np.arange(data.index.size))
+
+# plot results
+fig, ax = plt.subplots()
+data.ssh.plot(c="k", ax=ax)
+
+# years of interest
+x_years = [2, 10, 50, 100, 500]
+
+# repeat plotting of effective return levels for all years of interest
+for x in x_years:
+    _ = ax.plot(
+        data.index, estimate_return_level(1 - 1 / x, law_ns), label=f"{x}-year return level"
+    )
+
+ax.legend()
+ax.grid(True)
+ax.set_xlabel("Time (years)")
+ax.set_ylabel("Annual Maximum Sea Surface \nHeight Anomaly (mm)")
+ax.set_title("Scale as Function of Time")
